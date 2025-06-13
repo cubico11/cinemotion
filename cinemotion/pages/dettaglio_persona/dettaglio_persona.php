7453ee7adf280d5e1e181422afa91d2136ff3969
Fabio Cubeddu <cubeddufabio1@gmail.com>
2025-06-13 19:10:11 +0200

    Modifiche stile

--- cinemotion/pages/dettaglio_persona/dettaglio_persona.php
+++ cinemotion/pages/dettaglio_persona/dettaglio_persona.php
@@ -31,41 +31,42 @@ include '../../funzioni.php';
             </div>
         </div>
 
-        <div class="person-films-list">
-            <div id="toggle-buttons" class="toggle-buttons">
-                <?php
-                if($persona->getNFilmByRuolo($conn, "Attore") > 0){
-                    echo "<button id='btn-attore' onclick='showSection(\"attore\", this)'>Film da attore</button>";
-                }
-                if($persona->getNFilmByRuolo($conn, "Regista") > 0){
-                    echo "<button id='btn-regista' onclick='showSection(\"regista\", this)'>Film da regista</button>";
-                }
-                if($persona->getNFilmByRuolo($conn, "Sceneggiatore") > 0){
-                    echo "<button id='btn-sceneggiatore' onclick='showSection(\"sceneggiatore\", this)'>Film da sceneggiatore</button>";
-                }
-                ?>
-            </div>
+        <div id="toggle-buttons" class="toggle-buttons">
             <?php
-            if($persona->getNFilmByRuolo($conn, "Attore") > 0){
+            if ($persona->getNFilmByRuolo($conn, "Attore") > 0) {
+                echo "<button id='btn-attore' onclick='showSection(\"attore\", this)'>Film da attore</button>";
+            }
+            if ($persona->getNFilmByRuolo($conn, "Regista") > 0) {
+                echo "<button id='btn-regista' onclick='showSection(\"regista\", this)'>Film da regista</button>";
+            }
+            if ($persona->getNFilmByRuolo($conn, "Sceneggiatore") > 0) {
+                echo "<button id='btn-sceneggiatore' onclick='showSection(\"sceneggiatore\", this)'>Film da sceneggiatore</button>";
+            }
+            ?>
+        </div>
+
+        <div class="person-films-list">
+            <?php
+            if ($persona->getNFilmByRuolo($conn, "Attore") > 0) {
                 echo "<div id='attore' class='toggle-section' style='display: none;'>
-                    <div class='film-grid' style='padding-left: 0'>" .
-                        $persona->printFilmByRuolo($conn, 'Attore', '../dettaglio_film') .
+                    <div class='film-grid'>" .
+                    $persona->printFilmByRuolo($conn, 'Attore', '../dettaglio_film') .
                     "</div>
                 </div>";
             }
 
-            if($persona->getNFilmByRuolo($conn, "Regista") > 0){
+            if ($persona->getNFilmByRuolo($conn, "Regista") > 0) {
                 echo "<div id='regista' class='toggle-section' style='display: none;'>
-                    <div class='film-grid' style='padding-left: 0'>" .
-                        $persona->printFilmByRuolo($conn, 'Regista', '../dettaglio_film') .
+                    <div class='film-grid'>" .
+                    $persona->printFilmByRuolo($conn, 'Regista', '../dettaglio_film') .
                     "</div>
                 </div>";
             }
 
-            if($persona->getNFilmByRuolo($conn, "Sceneggiatore") > 0){
+            if ($persona->getNFilmByRuolo($conn, "Sceneggiatore") > 0) {
                 echo "<div id='sceneggiatore' class='toggle-section' style='display: none;'>
-                    <div class='film-grid' style='padding-left: 0'>" .
-                        $persona->printFilmByRuolo($conn, 'Sceneggiatore', '../dettaglio_film') .
+                    <div class='film-grid'>" .
+                    $persona->printFilmByRuolo($conn, 'Sceneggiatore', '../dettaglio_film') .
                     "</div>
                 </div>";
             }
@@ -77,22 +78,20 @@ include '../../funzioni.php';
 </body>
 <script>
     function attivaPrimoBottone() {
-        <?php 
-            //attiva il primo bottone tra i tre (attore, regista o sceneggiatore)
-            if($persona->getNFilmByRuolo($conn, "Attore") == 0 && $persona->getNFilmByRuolo($conn, "Regista") == 0) {
-                echo "document.getElementById(\"sceneggiatore\").style.display = '';
+        <?php
+        //attiva il primo bottone tra i tre (attore, regista o sceneggiatore)
+        if ($persona->getNFilmByRuolo($conn, "Attore") == 0 && $persona->getNFilmByRuolo($conn, "Regista") == 0) {
+            echo "document.getElementById(\"sceneggiatore\").style.display = '';
                 document.getElementById(\"btn-sceneggiatore\").classList.add('active');";
-            }
-            else if($persona->getNFilmByRuolo($conn, "Attore") == 0) {
-                echo "document.getElementById(\"regista\").style.display = '';
+        } else if ($persona->getNFilmByRuolo($conn, "Attore") == 0) {
+            echo "document.getElementById(\"regista\").style.display = '';
                 document.getElementById(\"btn-regista\").classList.add('active');";
-            }
-            else{
-                echo "document.getElementById(\"attore\").style.display = '';
+        } else {
+            echo "document.getElementById(\"attore\").style.display = '';
                 document.getElementById(\"btn-attore\").classList.add('active');";
-            }
+        }
         ?>
-        
+
     }
 
     function showSection(sectionId, button) {
@@ -113,6 +112,7 @@ include '../../funzioni.php';
         button.classList.add('active');
     }
 </script>
+
 </html>
 
 <?php $conn->close(); ?>
